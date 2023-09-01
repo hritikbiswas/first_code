d21dff6ce7f7f517b212113fbcc09a98ba131010
hritikbiswas <hrittikbiswas90@gmail.com>
2023-09-01 13:20:59 +0530

    added table of given number as well

--- EvenOrOdd.js
+++ EvenOrOdd.js
@@ -5,3 +5,10 @@ if(num%2==0){
 else{
     console.log(num,"is Odd number");
 }
+
+// "the bellow code is used for finding table of the given number"
+
+let no=3;
+for(let i=1;i<=10;i++){
+     console.log(no, "*",i,"=",no*i)
+}
